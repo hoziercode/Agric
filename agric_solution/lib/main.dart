3030e763f13213300b705a22e9550590906ca2b2
hoziercode <hoseasalako@gmail.com>
2023-04-14 12:59:24 +0100

    firts

--- agric_solution/lib/main.dart
+++ agric_solution/lib/main.dart
@@ -10,8 +10,14 @@ import './screens/SignUpScreens/signUp_screen4.dart';
 import './screens/Store_screen.dart';
 import './screens/purchase_screen.dart';
 import './screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
